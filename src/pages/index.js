b1c587c7bf53d8e158a550aa405d95d432ea4d65
Luke <Infinitybeond1@protonmail.com>
2022-08-12 21:52:22 -0500

    add footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/index.css';
-import { DiscordLogo, GithubLogo, NewspaperClipping, EnvelopeOpen } from 'phosphor-react'
+import { DiscordLogo, GithubLogo, NewspaperClipping, EnvelopeOpen, HeartStraight } from 'phosphor-react'
 import {Helmet} from 'react-helmet'
 <Helmet>
   <title>Infinitybeond1's site</title>
@@ -13,16 +13,22 @@ import {Helmet} from 'react-helmet'
 
 function Index() {
   return (
-    <div class="topnav">
-      <a className="active" href="~">Home</a>
-      <a className="tab" href="/projects">Projects</a>
-      <a className="tab" href="/technologies">Technologies</a>
-      <a className="tab" href="/about">About</a>
-      <a className="social" href="https://discordapp.com/channels/@me/704106773660827690/"> < DiscordLogo size={19} /> </a>
-      <a className="social" href="https://blog.infinitybeond1.tk/"> < NewspaperClipping size={19} /> </a>
-      <a className="social" href="mailto:Infinitybeond1@protonmail.com"> < EnvelopeOpen size={19} /> </a>
-      <a className="social" href="https://github.com/Infinitybeond1"> < GithubLogo size={19} /> </a>
-    </div>   
+    <>
+      <div class="topnav">
+        <a className="active" href="~">Home</a>
+        <a className="tab" href="/projects">Projects</a>
+        <a className="tab" href="/technologies">Technologies</a>
+        <a className="tab" href="/about">About</a>
+        <a className="social" href="https://discordapp.com/channels/@me/704106773660827690/"> < DiscordLogo size={19} /> </a>
+        <a className="social" href="https://blog.infinitybeond1.tk/"> < NewspaperClipping size={19} /> </a>
+        <a className="social" href="mailto:Infinitybeond1@protonmail.com"> < EnvelopeOpen size={19} /> </a>
+        <a className="social" href="https://github.com/Infinitybeond1"> < GithubLogo size={19} /> </a>
+      </div>
+
+      <div class="footer">
+        <p class="footer-txt"> Made with <HeartStraight size={13} weight="fill" /> using the articblush colorscheme and GatsbyJS </p>
+      </div>
+    </>
   );
 }
 
